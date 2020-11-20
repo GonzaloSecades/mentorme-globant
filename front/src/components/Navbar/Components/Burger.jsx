4e9f928f642524a77b761b20667ea4838bcd21b8
Tu Nombre <you@example.com>
2020-11-20 01:10:29 -0300

    vistas

---
+++ front/src/components/Navbar/Components/Burger.jsx
@@ -0,0 +1,24 @@
+function Burger(){
+return (
+
+<div id="menuToggle">
+    <input type="checkbox" />
+    <span></span>
+    <span></span>
+    <span></span>
+    <ul id="menu">
+      <a href="#"><li>Home</li></a>
+      <a href="#"><li>About</li></a>
+      <a href="#"><li>Info</li></a>
+      <a href="#"><li>Contact</li></a>
+    </ul>
+  </div>
+
+
+
+)
+
+}
+
+export default Burger
+
--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.scss';
-import App from './commponents/App';
+import App from './components/Landing/Landing';
 import { BrowserRouter, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Navbar from "./components/Navbar/Navbar"
+import Main from "./Main"
 
 ReactDOM.render(
     <BrowserRouter>
-      <Route path="/" component={App} />
+      <Route path="/" component={Main} />
     </BrowserRouter>,
   document.getElementById('root')
 );
