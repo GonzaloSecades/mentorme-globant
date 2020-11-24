8e04e8edf3175304d910589ce27ef08157774785
Aletartaglia <alejandro.tartaglia92@gmail.com>
2020-11-24 19:06:41 -0300

    merge2

--- front/src/components/Login/Login.jsx
+++ front/src/components/Login/Login.jsx
@@ -1,8 +1,8 @@
-import React,{useState} from 'react';
-import {useDispatch} from 'react-redux'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux'
 import { makeStyles, TextField, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import {login} from '../../redux/action-creators/currentUser'
+import { login } from '../../redux/action-creators/currentUser'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -30,7 +30,7 @@ const useStyles = makeStyles((theme) => ({
     "& .MuiInputBase-input": {
       width: "18rem",
     },
-  
+
     "& .MuiInputLabel-root.Mui-focused": {
       color: "#006400",
     },
@@ -42,6 +42,25 @@ const useStyles = makeStyles((theme) => ({
 
 function Login() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const [useEmail, setEmail] = useState("")
+  const [usePassword, setPassword] = useState("")
+
+  function onChange(e) {
+    console.log(e.target.value)
+    if (e.target.name === "email") {
+      setEmail(e.target.value)
+    } else {
+      setPassword(e.target.value)
+    }
+
+  }
+  function onSubmit(e) {
+    e.preventDefault()
+    dispatch(login(useEmail, usePassword))
+    console.log('ENTRO')
+  }
+
   return (
     <div>
       <div className="formContainerLogin">
@@ -91,51 +110,6 @@ function Login() {
               Create Your Account
             </Button>
           </Link>
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const [useEmail, setEmail] = useState("")
-    const [usePassword, setPassword] = useState("")
-
-    function onChange(e){
-        console.log(e.target.value)
-        if(e.target.name === "email"){
-            setEmail(e.target.value)
-        }else{
-            setPassword(e.target.value)
-        }
-        
-    }
-    function onSubmit(e){
-        e.preventDefault()
-        dispatch(login(useEmail,usePassword))
-        console.log('ENTRO')
-    }
-    
-
-    return (
-        <div>
-            <form onSubmit={onSubmit} className="formContainerLogin">
-                <p>Sign In</p>
-                <TextField className={classes.formLogininput} id="outlined-search" label=" Email *" name="email" type="text" variant="outlined" 
-                onChange={onChange}/>
-                <TextField className={classes.formLogininput} id="outlined-password-input" label=" Password *" name="password" type="password" autoComplete="current-password" variant="outlined"
-                onChange={onChange}
-                />
-                <Link to="#" className="forgotpassword" href="#">Lost Your Password ?</Link>
-                <div className="buttonLoginContainer">
-                    <Button style={{ backgroundColor: "rgba(18,41,68,1)", borderRadius: "20px", width: "100%", margin: "20px auto", }} className={classes.buttonSignin} variant="contained" color="primary"  onClick={onSubmit}>
-                        Sign in
-                </Button>
-                    <Link to="/register" style={{ textDecoration: 'none' }}>
-                        <Button style={{ color: "rgba(18,41,68,1)", border: "2px solid rgba(18,41,68,1)" }} className="buttoncreateacc" variant="outlined" color="primary">
-                            Create Your Account
-                </Button>
-                    </Link>
-
-                </div>
-
-            </form>
-
         </div>
       </div>
     </div>
