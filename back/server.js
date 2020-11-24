65c6ddf6c79d5cc0371237fdf6e007914b8d38e6
gonzalo <gsecades13@gmail.com>
2020-11-24 12:30:09 -0300

    cambios vito

--- back/server.js
+++ back/server.js
@@ -1,6 +1,6 @@
 require("./config/index");
 const express = require("express");
-const path = require('path')
+const path = require("path");
 const volleyball = require("volleyball");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
@@ -11,14 +11,8 @@ app.use(volleyball);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 //Routes
 app.use("/api", routes);
 
-<<<<<<< HEAD
 app.listen(8000, () => console.log("SERVER LISTENING AT PORT 8000"));
-=======
-
-app.listen(8000, ()=>console.log("SERVER LISTENING AT PORT 8000"))
->>>>>>> 855cb5af6c76bb7ca84f0b9d554003cb5a91b7e4
