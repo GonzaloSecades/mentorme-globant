fda00601634032c7e9a3cec43ee11583459876f2
gonzalo <gsecades13@gmail.com>
2020-11-24 23:14:07 -0300

    cambios gonza skill

--- front/src/components/Register/UserForm.jsx
+++ front/src/components/Register/UserForm.jsx
@@ -1,71 +1,81 @@
-import React from "react"
-import  { useState } from "react"
-import FormUserNew from "./components/FormUserNew"
-import FormUserData from "./components/FormUserData"
-import FormUserSkills from "./components/FormUserSkills"
-import FormUserSave from "./components/FormUserSave"
-import FormUserSuccsess from "./components/FormUserSuccsess"
+import React from "react";
+import { useState, useEffect } from "react";
+import FormUserNew from "./components/FormUserNew";
+import FormUserData from "./components/FormUserData";
+import FormUserSkills from "./components/FormUserSkills";
+import FormUserSave from "./components/FormUserSave";
+import FormUserSuccsess from "./components/FormUserSuccsess";
 
-function UserForm(){
-const [step, setStep] = useState(1)
-const [user, setUser] = useState({
-  email: "",
-  password: "",
-  firstName: "",
-  lastName: "",
-  country: "",
-  phoneNumber: "",
-  languages: [],
-  avatar: "",
-  skills: [],
-  skillsToLearn: [],
-  skillsToTeach: [],
-  mentors: [],
-  mentees: [],
-  isAdmin: false,
-})
+function UserForm() {
+  const [step, setStep] = useState(1);
+  const [skills, setSkills] = useState([]);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    country: "",
+    phoneNumber: "",
+    languages: [],
+    avatar: "",
+    //skills: [],
+    skillsToLearn: [],
+    skillsToTeach: [],
+    mentors: [],
+    mentees: [],
+    isAdmin: false,
+  });
 
-const nextStep = ()=>{
-    setStep(step + 1) 
-    }
+  const nextStep = () => {
+    setStep(step + 1);
+  };
 
-const prevStep = ()=>{
-    setStep(step - 1) 
-    }
+  const prevStep = () => {
+    setStep(step - 1);
+  };
 
-const handleChange = input => e =>{
-console.log(e.target.value)
-}   
+  const handleChange = (input) => (e) => {
+    console.log(e.target.value);
+  };
 
-    switch(step){
-        case 1: 
-        return <FormUserNew 
-        nextStep={nextStep}
-        handleChange={handleChange}
+  const submitMySkills = (skillsArray) => {
+    setSkills(skillsArray);
+  };
+
+  useEffect(() => console.log(skills), [skills]);
+  switch (step) {
+    case 1:
+      return <FormUserNew nextStep={nextStep} handleChange={handleChange} />;
+    case 2:
+      return (
+        <FormUserData
+          nextStep={nextStep}
+          prevStep={prevStep}
+          handleChange={handleChange}
         />
-        case 2: 
-        return <FormUserData   
-        nextStep={nextStep}
-        prevStep={prevStep}
-        handleChange={handleChange}
+      );
+    case 3:
+      return (
+        <FormUserSkills
+          nextStep={nextStep}
+          prevStep={prevStep}
+          handleChange={handleChange}
+          submitMySkills={submitMySkills}
         />
-        case 3: 
-        return <FormUserSkills  
-        nextStep={nextStep}
-        prevStep={prevStep}
-        handleChange={handleChange} />
-        case 4: 
-        return <FormUserSave    
-        nextStep={nextStep}
-        prevStep={prevStep}
-        handleChange={handleChange}/>
-        case 5: 
-        return <FormUserSuccsess 
-        prevStep={prevStep}
-        handleChange={handleChange}/>
-   
-    }
-
+      );
+    case 4:
+      return (
+        <FormUserSave
+          nextStep={nextStep}
+          prevStep={prevStep}
+          handleChange={handleChange}
+        />
+      );
+    case 5:
+      return (
+        <FormUserSuccsess prevStep={prevStep} handleChange={handleChange} />
+      );
+  }
 }
 
-export default UserForm
+export default UserForm;
--- front/src/components/Register/components/FormUserSkills.jsx
+++ front/src/components/Register/components/FormUserSkills.jsx
@@ -1,31 +1,127 @@
-import React from "react"
+import React, { useState } from "react";
+import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import { Button } from "@material-ui/core";
 
-function FormUserSkills(props){
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
-    const next = (e) =>{
-        e.preventDefault()
-        props.nextStep()
-    
-    }
+function FormUserSkills(props) {
+  const next = (e) => {
+    e.preventDefault();
+    props.nextStep();
+  };
 
-    const prev = (e) =>{
-        e.preventDefault()
-        props.prevStep()
-    
-    }
+  const prev = (e) => {
+    e.preventDefault();
+    props.prevStep();
+  };
 
-    return(
-     <div>
-          <p>STEP 3 SKILLS...</p>
-            <button onClick={next} >Continuar<br/></button>
-            <button  onClick={prev} >Volver<br/></button>
-     </div>
+  return (
+    <div>
+      <p>My Skills</p>
 
-
-    ) 
-
-
-
+      <Autocomplete
+        onChange={(event, value) => props.submitMySkills(value)} // prints the selected value
+        multiple
+        id="checkboxes-tags-demo"
+        options={top100Films}
+        disableCloseOnSelect
+        getOptionLabel={(option) => option.name}
+        renderOption={(option, { selected }) => (
+          <React.Fragment>
+            <Checkbox
+              icon={icon}
+              checkedIcon={checkedIcon}
+              style={{ marginRight: 8 }}
+              checked={selected}
+            />
+            {option.name}
+          </React.Fragment>
+        )}
+        style={{ width: 500 }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            label="Checkboxes"
+            placeholder="Favorites"
+          />
+        )}
+      />
+    </div>
+  );
 }
-
-export default FormUserSkills
+export default FormUserSkills;
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+  {
+    _id: "5fb818a7ae6e9a6349017102",
+    name: "mongo",
+    keywords: ["backend, odm"],
+    __v: 0,
+  },
+  {
+    _id: "5fb818a7ae6e9a6349017103",
+    name: "sequelize",
+    keywords: ["backend, orm"],
+    __v: 0,
+  },
+  {
+    _id: "5fb818a7ae6e9a6349017104",
+    name: "node",
+    keywords: ["front end framework"],
+    __v: 0,
+  },
+  {
+    _id: "5fb818a7ae6e9a6349017105",
+    name: "docker",
+    keywords: ["devops"],
+    __v: 0,
+  },
+  {
+    _id: "5fb818a7ae6e9a6349017106",
+    name: "kubernetes",
+    keywords: ["devops"],
+    __v: 0,
+  },
+  {
+    _id: "5fb818a7ae6e9a6349017107",
+    name: "openshift",
+    keywords: ["devops"],
+    __v: 0,
+  },
+  {
+    _id: "5fb818a7ae6e9a6349017108",
+    name: "jenkins",
+    keywords: ["devops"],
+    __v: 0,
+  },
+  {
+    _id: "5fb818a7ae6e9a6349017109",
+    name: "ansible",
+    keywords: ["devops"],
+    __v: 0,
+  },
+  {
+    _id: "5fb818a7ae6e9a6349017110",
+    name: "terraform",
+    keywords: ["devops"],
+    __v: 0,
+  },
+  {
+    _id: "5fb818a7ae6e9a6349017111",
+    name: "postgres",
+    keywords: ["database"],
+    __v: 0,
+  },
+  {
+    _id: "5fb818a7ae6e9a6349017112",
+    name: "mysql",
+    keywords: ["database"],
+    __v: 0,
+  },
+];
