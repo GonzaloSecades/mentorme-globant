ceae1eeb53843f421e428e58641521b83da08c20
Aletartaglia <alejandro.tartaglia92@gmail.com>
2020-11-25 15:34:07 -0300

    hay transition

--- front/src/Main.js
+++ front/src/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Switch, useLocation } from "react-router-dom";
+import { Route, Switch, useLocation, useHistory } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 //STYLES
@@ -12,34 +13,53 @@ import Login from "./components/Login/Login";
 import Menu from "./components/Menu/Menu";
 import MyProfile from "./components/MyProfile/MyProfile";
 
-import {me} from './redux/action-creators/currentUser'
+import { me } from './redux/action-creators/currentUser'
 
 function Main() {
   const dispatch = useDispatch()
   const location = useLocation().pathname;
-
+  const history = useHistory()
   //HOOK PERSISTENCIA DE SESION
-  React.useEffect(() => {
+  useEffect(() => {
     if (document.cookie) {
       const token = document.cookie
         .split('; ')
         .find(row => row.startsWith('token'))
         .split('=')[1];
       dispatch(me(token))
+      /*   .then(() => {
+          setTimeout(() => {
+            history.push("/myprofile")
+          }, 1000);
+        }) */
+    } else {
+      setTimeout(() => {
+        history.push("/login")
+      }, 1000);
     }
+
   }, [])
 
 
   return (
     <div className="order">
       {location === "/" ? null : <Navbar />}
-      <div>
-        <Switch>
-          <Route exact path="/" component={Landing}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/register" component={UserForm}></Route>
-          <Route path="/myprofile" component={MyProfile} />
-        </Switch>
+      <div >
+        <Route render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition
+              key={location.key}
+              timeout={2000}
+              classNames="fade">
+              <Switch location={location} >
+                <Route exact path="/" component={Landing}></Route>
+                <Route path="/login" component={Login}></Route>
+                <Route path="/register" component={UserForm}></Route>
+                <Route path="/myprofile" component={MyProfile} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )} />
       </div>
       {location === "/" || "/register" ? null : <Menu />}
     </div>
